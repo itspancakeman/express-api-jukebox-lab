e63856b685aa60f8bb174fb4fef5a050b6dbef9e
itspancakeman <collinjst.onge@gmail.com>
2024-07-17 16:00:56 -0400

    feat: added edit routes for tracks

--- routes.js
+++ routes.js
@@ -4,7 +4,8 @@ const router = express.Roputer();
 
 router.get('/tracks', async (req, res) => {
     const tracks = await Track.find();
-    res.send(tracks);
+    res.status(200);
+    res.send('OK', tracks);
 });
 
 router.post('/tracks', async (req, res) => {
@@ -13,12 +14,38 @@ router.post('/tracks', async (req, res) => {
         artist: req.body.artist,
     });
     await track.save();
-    res.send(track);
+    res.status(201);
+    res.send('created: ', track);
 });
 
 router.get('/tracks/:id', async (req, res) => {
+    try {
     const track = await Post.findOne({ _id: req.params.id });
     res.send(track);
+    } catch {
+        res.status(500);
+        res.send({ error: "Internal server error" });
+    }
+});
+
+router.patch('/tracks/:id', async (req, res) => {
+    try {
+        const track = await Track.findOne({ _id: req.params.id })
+
+        if (req.body.title) {
+            track.title = req.body.title
+        }
+
+        if (req.body.artist) {
+            track.artist = req.body.artist
+        }
+
+        await track.save();
+        res.send(track);
+    } catch {
+        res.status(500);
+        res.send({ error: 'Internal server error' });
+    }
 });
 
 module.exports = router;
