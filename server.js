3e89048b9a163e8dab1b02bd0213dbb068450f72
itspancakeman <collinjst.onge@gmail.com>
2024-07-16 23:32:01 -0400

    feat: more to do

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const app = express();
+const router = express.Router();
 const session = require('express-session');
 const PORT = process.env.PORT || 3000;
 const methodOverride = require('method-override');
 const axios = require('axios');
 const cors = require('cors');
+const { Track } = require('./models');
 
 // ===== MIDDLEWARE ==== 
 app.set('view engine', 'ejs');
@@ -15,7 +17,15 @@ app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(cors())
 
-
+router.get('/tracks', async (req, res) => {
+    try {
+        const tracks = await Track.find();
+        res.json(tracks);
+    } catch (error) {
+        res.status(500)
+            .json({ message: 'Inernal server error' });
+    }
+});
 
 
 
@@ -26,4 +36,4 @@ const server = app.listen(PORT, () => {
     console.log('listening at PORT ', PORT);
 });
 
-module.exports = server;
+module.exports = server, router;
